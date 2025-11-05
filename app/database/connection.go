98b1b57d9b2f3aa652ce1e7a4deb3fc6275b0fbe
Masaru Fukazawa <upw.ponsuke@gmail.com>
2025-11-06 00:11:39 +0900

    DB接続処理を実装

---
+++ app/database/connection.go
@@ -0,0 +1,35 @@
+package database
+
+import (
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type DB struct {
+	connection *gorm.DB
+}
+
+func (db *DB) Disconnection() error {
+	sqlDB, err := db.connection.DB()
+
+	if err != nil {
+		return err
+	}
+
+	return sqlDB.Close()
+}
+
+func (db *DB) GetConnection() *gorm.DB {
+    return db.connection
+}
+
+func NewDBConnection(path string) (*DB, error) {
+
+	connection, err := gorm.Open(sqlite.Open(path), &gorm.Config{})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &DB{connection: connection}, nil
+}
